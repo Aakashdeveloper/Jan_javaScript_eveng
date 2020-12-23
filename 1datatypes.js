9a878943fde74ef9f2e58c2dfbc03c56c840a125
aakashdeveloper <ahanda205@gmail.com>
2020-12-23 20:37:47 +0530

    a

---
+++ 1datatypes.js
@@ -0,0 +1,118 @@
+Es5 > Ecma Script
+es6
+es7
+
+String 
+Number
+Boolean
+
+string = "hii" 'hello' '32543' 'true' "3252frg"
+number = 6 476437678 45.454 .4534 45243435.464564545
+boolean = true/false
+null
+undefined
+
+///////
+Es5
+///Declare variable////
+
+var a = "Hiii"
+var b = 10
+var c = true
+
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = 10.44
+undefined
+typeof(d)
+"number"
+var e = "10"
+undefined
+typeof(e)
+"string"
+
+///////////////
+var a = 10
+var b = 20
+
+
+a+b (add)
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+4%2  > modules(remender value)
+0
+4%3
+1
+4%4
+0
+
+2%4
+2  
+3%4
+3
+
+>>>>>>>>>modules of smaller to higher is always smaller
+
+
+
+var a = "Hi "
+var b = "javaScript"
+undefined
+a+b (concat)
+"Hi javaScript"
+a-b
+NaN
+a*b 
+NaN
+a/b
+NaN  > Not a number
+
+var a = "Hi "
+undefined
+var b = 10
+undefined
+a+b
+"Hi 10"
+b+a
+"10Hi "
+a-b
+NaN
+a/b
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+//////////////
+10+"20"+30-1
+102029
+
+10+20+"30"-1
+3029
+
+"10"+20+30-1
+102029
