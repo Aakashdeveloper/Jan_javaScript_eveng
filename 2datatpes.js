57710e262e4a35d00f8245c7a11ceb7f8e742de0
aakashdeveloper <ahanda205@gmail.com>
2020-12-25 20:32:39 +0530

    a

---
+++ 2datatpes.js
@@ -0,0 +1,55 @@
+var a = true
+var b = true
+a+b 
+2
+true+false
+1
+false+false
+0
+true+10
+11
+
+true === 1
+false === 0
+"a"+true
+"atrue"
+"a"+true
+"atrue"
+"a"-true
+NaN
+"10"-true
+9
+
+var a = "10"
+var b = "20"
+a+b
+"1020"
+parseInt(a)+parseInt(b)
+30
+
+var a = "10.11"
+undefined
+parseInt(a)
+10
+parseFloat(a)
+10.11
+var b = "11"
+undefined
+parseFloat(b)
+11
+parseFloat(a)
+10.11
+
+//es6
+Number(a)
+10.11
+Number(b)
+11
+
+var a = "egreg"
+isNaN(a)
+true
+
+var b = 10
+isNaN(b)
+false
